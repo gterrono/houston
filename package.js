e4fafd11d15b0a0b7f88c37ec42118d36c39771c
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-06-30 04:00:16 -0700

    navbar

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.on_use(function(api) {
   // load html first, https://github.com/meteor/meteor/issues/282
   api.add_files('client/main.html', 'client');
 
-  api.add_files(['client/style.css', 'client/admin_login.html', 'client/db_view.html', 'client/collection_view.html', 'client/document_view.html'], 'client');
+  api.add_files(['client/style.css', 'client/admin_login.html', 'client/db_view.html', 'client/collection_view.html', 'client/document_view.html', 'client/admin_navbar.html'], 'client');
 
   api.add_files(['client/router.coffee', 'client/admin_login.coffee', 'client/collection_view.coffee', 'client/document_view.coffee'], 'client');
   
