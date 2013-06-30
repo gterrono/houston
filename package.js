ac08614ca6a9e88249d0baa7f9ef54346ba77e9e
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-06-30 02:25:03 -0700

    Added dependencies

--- package.js
+++ package.js
@@ -4,10 +4,18 @@ Package.describe({
 
 Package.on_use(function(api) {
   api.use("coffeescript", ['client', 'server']);
+  api.use('accounts-base', ['client', 'server']);
+  api.use('accounts-password', ['client', 'server']);
+  api.use('bootstrap', ['client', 'server']);
+  api.use(['templating'], 'client');
+
+  // load html first, https://github.com/meteor/meteor/issues/282
+  api.add_files('client/main.html', 'client');
+
+  api.add_files(['client/style.css', 'client/admin_login.html', 'client/db_view.html', 'client/collection_view.html', 'client/document_view.html'], 'client');
 
   api.add_files(['client/router.coffee', 'client/admin_login.coffee', 'client/collection_view.coffee', 'client/document_view.coffee'], 'client');
   
-  api.add_files(['client/style.css', 'client/main.html', 'client/admin_login.html', 'client/db_view.html', 'client/collection_view.html', 'client/document_view.html'], 'client');
 
   api.add_files('lib/collections.coffee', ['client', 'server']);
   
