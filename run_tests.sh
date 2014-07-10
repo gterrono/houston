7827c58bb2fae3263baf650125e66e81442be25a
Alexey Komissarouk <alexey@alexeymk.com>
2014-07-09 20:17:02 -0700

    omg, tests run and I understand how kill_mrt works

--- run_tests.sh
+++ run_tests.sh
@@ -2,22 +2,28 @@
 
 function kill_mrt {
   # Kills mrt and all the child processes
-  # From http://stackoverflow.com/a/15139734/2624068
-  # kill -- -$(ps -o pgid= $! | grep -o [0-9]*) 2> /dev/null > /dev/null 3> /dev/null
-
-  #... yes, but this actually works.
-  killall node >/dev/null 2>/dev/null 3>/dev/null
+  MRT_ID=$1
+  # print all process id/group ids for current user
+  # | only find ones with our meteor id in them (as pid or pgid)
+  # | fix formatting so we get all pids we'll need to 'kill'
+  # | kill each one
+  # > don't show normal output to user
+  ps -o pid,pgid | grep $MRT_ID | cut -f 1 -d " " | xargs kill > /dev/null
 }
 
 function run_test {
   TEST_FILE=$1
   CUSTOM_TEST_ARGS="$2"
   cd test/app
-  mrt --port=3500 $CUSTOM_TEST_ARGS > /dev/null &
+  # clear any past DB state
+  rm -r .meteor/local > /dev/null
+  # meteor, not mrt, so there are fewer child processes to chase down
+  meteor --port=3500 $CUSTOM_TEST_ARGS > /dev/null &
+  MRT_ID=$!
   cd ../..
   casperjs test test/$TEST_FILE
   echo "casper tests done running"
-  kill_mrt
+  kill_mrt $MRT_ID
 }
 
 if [! hash python >/dev/null 2>&1 ]
@@ -27,6 +33,5 @@ then
   echo "or '[sudo] npm install -g casperjs'          (npm)"
 fi
 
-kill_mrt # just in case we have some previous processes running
 run_test test_base.coffee ""
 run_test test_custom_root_route.coffee "--settings settings.json"
