7e180e618d8846b569c5ac8b1c4b162256d67fbf
Alexey Komissarouk <alexey@alexeymk.com>
2014-09-16 16:39:37 -0600

    removing bootstrap js as dependency

--- package.js
+++ package.js
@@ -41,7 +41,6 @@ Package.on_use(function(api) {
   api.add_files(['lib/collections.coffee', 'lib/shared.coffee', 'lib/menu.coffee'], ['client', 'server']);
 
   api.add_files([
-    'client/third-party/bootstrap.min.js',
     'client/lib/shared.coffee',
     'client/custom_template_view.coffee',
     'client/router.coffee', 'client/admin_login.coffee',
