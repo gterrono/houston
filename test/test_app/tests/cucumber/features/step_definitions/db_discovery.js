6971a9fa9c1319a5dee6bf6008f27bce041cc80f
Sam Corcos <sam@corcos.io>
2015-08-22 19:03:01 +0200

    adding the test specs

--- test/test_app/tests/cucumber/features/step_definitions/db_discovery.js
+++ test/test_app/tests/cucumber/features/step_definitions/db_discovery.js
@@ -1,51 +1,46 @@
-// 'use strict';
-// // You can include npm dependencies for support files in tests/cucumber/package.json
-// var _ = require('underscore');
-// // You can use normal require here, cucumber is NOT run in a Meteor context (by design)
-// var url = require('url');
-
 /*
 NOTE: this.server.call runs synchronously.
 If you want it to run async, you must use
 this.server.callAsync
+
+var _ = require('underscore');
 */
+//
+// module.exports = function () {
+//   this.Given(/^I am an admin$/, function (callback) {
+//     // // First: reset accounts
+//     // this.server.call('test/reset');
+//     //
+//     // // Second: create an account
+//     // var id = this.server.call('test/createUser', "bob", "password");
+//     //
+//     // // Third: add user to admin
+//     // this.server.call('_houston_make_admin', id)
+//     //
+//     // // Third: login
+//     // // this.client.call(Meteor.loginWithPassword('bob', 'password'))
+//     // // this.client.execute(Meteor.loginWithPassword('bob', 'password'))
+//     // client.execute(function() {
+//     //   // Meteor.loginWithPassword('bob', 'password')
+//     //   console.log(this);
+//     // })
+//
+//     // Fourth:
+//     // Check to make sure the account is an admin
+//
+//     callback.pending();
+//
+//   });
+//
+// };
 
 module.exports = function () {
   this.Given(/^I am an admin$/, function (callback) {
-    // First: reset accounts
-    this.server.call('test/reset');
-
-    // Second: create an account
-    var id = this.server.call('test/createUser', "bob", "password");
-
-    // Third: add user to admin
-    this.server.call('_houston_make_admin', id)
-
-    // Third: login
-    // this.client.call(Meteor.loginWithPassword('bob', 'password'))
-    // this.client.execute(Meteor.loginWithPassword('bob', 'password'))
-    client.execute(function() {
-      // Meteor.loginWithPassword('bob', 'password')
-      console.log(this);
-    })
-
-    // Fourth:
-    // Check to make sure the account is an admin
-
-  });
-
-  this.Given(/^I am on the route "([^"]*)"$/, function (arg1, callback) {
     // Write code here that turns the phrase above into concrete actions
     callback.pending();
   });
 
-
-  this.Given(/^I have collections$/, function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback.pending();
-  });
-
-  this.When(/^I sign in$/, function (callback) {
+  this.Given(/^I have more than one collection$/, function (callback) {
     // Write code here that turns the phrase above into concrete actions
     callback.pending();
   });
@@ -55,13 +50,38 @@ module.exports = function () {
     callback.pending();
   });
 
+  this.Given(/^I am a developer$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^some of my collections were not discovered$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
   this.When(/^I add an empty collection$/, function (callback) {
     // Write code here that turns the phrase above into concrete actions
     callback.pending();
   });
 
-  this.Then(/^I should have access to my the collection I just added$/, function (callback) {
-  // Write code here that turns the phrase above into concrete actions
-  callback.pending();
-});
-};
+  this.Then(/^the admin should have access to my the collection I just added$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^I should not see users or houston_admin$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^I add users and houston_admin$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^the admin should have access to those collections$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+}
