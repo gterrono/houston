ff87bb899cbc58056f0ba93cdfb6290503b7b48e
Alexey Komissarouk <alexey@alexeymk.com>
2015-11-05 14:55:31 +0900

    basic version of test app that uses iron router

---
+++ test/route-test-app/route-test-app.js
@@ -0,0 +1,27 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Router.route('/routeme', function () {
+      this.render('hello');
+  });
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
